795031021a48d37accf99b1a94c35f9d232a0609
MarinaLakhneva <mlakhneva@mail.ru>
2023-12-18 16:21:18 +0300

    рекурсия

--- swift/main.swift
+++ swift/main.swift
@@ -1,17 +1,27 @@
-let word = "newprogramminglanguages"
-var isPalindrome = true
-
-for i in 0..<word.count/2 {
-    let start = word.index(word.startIndex, offsetBy: i)
-    let end = word.index(word.endIndex, offsetBy: -i-1)
-    if word[start] != word[end] {
-        isPalindrome = false
-        break
-    }
-}
-
-if isPalindrome {
-    print("YES")
-} else {
-    print("NO")
+func isPalindrome(word: String) -> Bool { 
+    if (word.count <= 1) { 
+        return true 
+         
+    } 
+    else { 
+        let first = word.first! 
+        let last = word.last! 
+        if first != last { 
+            return false 
+        } 
+        else { 
+            let start = word.index(after: word.startIndex) 
+            let end = word.index(before: word.endIndex) 
+            let substring = word[start..<end] 
+            return isPalindrome(word: String(substring)) 
+        } 
+    } 
+} 
+ 
+let word = "racecar" 
+if isPalindrome(word: word) { 
+    print("YES") 
+} 
+else { 
+    print("NO") 
 }
